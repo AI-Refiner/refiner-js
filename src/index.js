479d564e44d895b70613c2f01a6caa0a603e947f
Alex <alexdaro34@gmail.com>
2023-08-13 15:40:55 -0700

    0.1.7 export integrations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 export { Embeddings } from "./embeddings.js";
 export { Loaders } from "./loaders.js";
 export { Transformers } from "./transformers.js";
+export { RefinerOpenAIClient } from "./integrations/refinerOpenAI.js";
+export { RefinerPineconeClient } from "./integrations/refinerPinecone.js";
+export { RefinerLangchainClient } from "./integrations/refinerLangchain.js";

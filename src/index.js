79e806d9d2ebc7f8408dfd45fa0b7d9ff804c08c
Alex <alexdaro34@gmail.com>
2023-08-21 22:24:28 -0700

    0.1.9 - indexes module for creating pinecone indexes.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { Embeddings } from "./embeddings.js";
+export { Indexes } from "./indexes.js";
 export { Loaders } from "./loaders.js";
 export { Transformers } from "./transformers.js";
 export { RefinerOpenAIClient } from "./integrations/refinerOpenAI.js";
